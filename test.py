2e2ac90e919ee52d196346f380e4660615e82256
Nyk00s <nikodemfal@o2.pl>
2026-03-23 17:08:30 +0100

    init commit with basic ttp and test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import socket
+import json
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9000))
+
+s.send((json.dumps({"action": "something"}) + '\n').encode())
+
+print(json.loads(s.recv(1024).decode()))
---
+++ ttp.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+import json
+import datetime
+
+HOST, PORT = "0.0.0.0", 9000
+
+
+def log(msg: str):
+    print(f"[{datetime.datetime.now().strftime('%d.%m.%y %H:%M:%S')}] " + msg)
+
+
+def handle_request(data: dict) -> dict:
+    log("client has sent something")
+    return {"status": "ok", "echo": data}
+    
+
+
+def handle_client(conn: socket.socket, addr):
+    try:
+        bytes_data = b""
+        while True:
+            chunk = conn.recv(4096)
+            if not chunk:
+                break
+            bytes_data += chunk
+            if bytes_data.endswith(b'\n'):
+                break
+
+        json_data = json.loads(bytes_data.decode().strip())
+        response = handle_request(json_data)
+        conn.sendall((json.dumps(response) + '\n').encode())
+    except Exception as e:
+        log(f"Error {addr} : {e}")
+    finally:
+        conn.close()
+
+
+def main():
+    print("start ttp")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen(10)
+        while True:
+            conn, addr = s.accept()
+            t = threading.Thread(target=handle_client, args=(conn, addr), daemon=True)
+            t.start()
+
+if __name__ == "__main__":
+    main()
